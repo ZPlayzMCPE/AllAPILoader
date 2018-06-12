4d94b9ac2f3306a74651bc5e1bb10394f519a8d9
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-06-12 17:27:19 +0100

    Update Main.php

--- src/TheNewHEROBRINE/AllAPILoader/Main.php
+++ src/TheNewHEROBRINE/AllAPILoader/Main.php
@@ -7,7 +7,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\plugin\PluginLoadOrder;
 use TheNewHEROBRINE\AllAPILoader\Loaders\AllFolderPluginLoader;
 use TheNewHEROBRINE\AllAPILoader\Loaders\AllPharPluginLoader;
-use TheNewHEROBRINE\AllAPILoader\Loaders\AllScriptPluginLoader;
 
 class Main extends PluginBase {
 
@@ -19,7 +18,7 @@ class Main extends PluginBase {
         if ($this->getServer()->getPluginManager()->getPlugin("DevTools") instanceof Plugin or $this->getServer()->getPluginManager()->getPlugin("FolderPluginLoader") instanceof Plugin)
             $this->getServer()->getPluginManager()->registerInterface(AllFolderPluginLoader::class);
 
-        $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getPluginPath(), [AllPharPluginLoader::class, AllScriptPluginLoader::class, AllFolderPluginLoader::class]);
+        $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getPluginPath(), [AllPharPluginLoader::class, AllFolderPluginLoader::class]);
         $this->getServer()->enablePlugins(PluginLoadOrder::STARTUP);
     }
 }
